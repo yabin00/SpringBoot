d228e393e96acc0e29fa2859b31747214daaa293
张亚彬 <yabin0618abc@outlook.com>
2024-03-20 15:09:03 +0800

    Default Changelist

---
+++ Test01Application06/src/main/java/org/itheima/ManualChapter01Application06.java
@@ -0,0 +1,11 @@
+package org.itheima;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualChapter01Application06 {
+    public static void main(String[] args) {
+        SpringApplication.run(ManualChapter01Application06.class, args);
+
+    }
+}
